aaa82e83c9684c9a1f4b2b66491c50384b60e969
Amr Taha <amrshiha2@gmail.com>
2022-09-01 20:52:22 +0200

    adding select date & time

--- src/components/home/Seats.tsx
+++ src/components/home/Seats.tsx
@@ -7,8 +7,8 @@ export const Seats = () => {
   const { movieTitle } = useParams();
 
   return (
-    <div className="px-4 pb-32">
-      <div className="flex items-center relative">
+    <div className="-mb-20">
+      <div className="px-4 flex items-center relative">
         <HiChevronLeft
           onClick={() => navigate(-1)}
           className="cursor-pointer text-5xl p-3"
--- src/components/home/SeatsSelection.tsx
+++ src/components/home/SeatsSelection.tsx
@@ -1,8 +1,9 @@
+import { Picker } from "./Picker";
 import { SelectSeats } from "./SelectSeats";
 
 export const SeatsSelection = () => {
   return (
-    <div className="my-10">
+    <div className="mt-10">
       <div className="mb-12 w-[90%] h-9 mx-auto overflow-hidden flex justify-center">
         <div className="min-w-[150vw] h-[150vw] border-4 border-orange rounded-full"></div>
       </div>
@@ -21,6 +22,21 @@ export const SeatsSelection = () => {
         <div className="flex items-center space-x-2">
           <div className="w-4 h-4 rounded-full bg-[#333]"></div>
           <p className="text-xs">Reserved</p>
+        </div>
+      </div>
+
+      <div className="mt-16 py-10 rounded-t-[2.5rem] bg-[#333] flex flex-col items-center">
+        <p className="font-semibold text-lg">Select Date and Time</p>
+        <Picker />
+
+        <div className="px-8 w-full flex items-center justify-between">
+          <div>
+            <p className="text-sm opacity-40">Total Price</p>
+            <p className="font-semibold text-xl">$43.55</p>
+          </div>
+          <button className="py-[25px] px-[50px] rounded-full bg-orange font-semibold text-black z-10">
+            Book Ticket
+          </button>
         </div>
       </div>
     </div>
