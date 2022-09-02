d6a2e7cd36b853c1ad15123758f911f12296d9f4
Amr Taha <amrshiha2@gmail.com>
2022-09-02 23:49:47 +0200

    adding dates and times to data

--- src/components/home/Seats.tsx
+++ src/components/home/Seats.tsx
@@ -13,6 +13,47 @@ export const Seats = () => {
       imgUrl: "/films/minions.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod et tempora nulla ad eos adipisci! Similique adipisci ea libero, magni harum vitae delectus accusamus omnis tempora temporibus esse asperiores deserunt ab dolore reiciendis aspernatur expedita labore maiores ad exercitationem nisi, molestias tenetur? Eos asperiores iure libero unde obcaecati incidunt adipisci molestiae ex. Distinctio, sapiente animi quod, incidunt sed quasi consequuntur temporibus quia, alias a laborum. Tenetur suscipit quas vero recusandae repudiandae sunt voluptate animi est reprehenderit, officia repellendus eveniet cum voluptatum rerum aspernatur. Ipsa laboriosam animi quas doloribus id cumque sapiente recusandae accusamus! Deleniti accusantium nam consectetur ex molestias?",
+      dates: [
+        {
+          id: "1",
+          date: { month: "Mar", day: 6 },
+        },
+        {
+          id: "2",
+          date: { month: "Mar", day: 7 },
+        },
+        {
+          id: "3",
+          date: { month: "Mar", day: 8 },
+        },
+        {
+          id: "4",
+          date: { month: "Mar", day: 9 },
+        },
+        {
+          id: "5",
+          date: { month: "Mar", day: 10 },
+        },
+        {
+          id: "6",
+          date: { month: "Mar", day: 11 },
+        },
+        {
+          id: "7",
+          date: { month: "Mar", day: 12 },
+        },
+      ],
+      times: [
+        { id: "1", time: "06:00" },
+        { id: "2", time: "07:30" },
+        { id: "3", time: "08:00" },
+        { id: "4", time: "09:30" },
+        { id: "5", time: "10:30" },
+        { id: "6", time: "12:00" },
+        { id: "7", time: "13:00" },
+        { id: "8", time: "14:30" },
+        { id: "9", time: "15:30" },
+      ],
       seats: [
         { y: 1, x: 3 },
         { y: 2, x: 3 },
@@ -39,6 +80,47 @@ export const Seats = () => {
       imgUrl: "/films/minions.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod et tempora nulla ad eos adipisci! Similique adipisci ea libero, magni harum vitae delectus accusamus omnis tempora temporibus esse asperiores deserunt ab dolore reiciendis aspernatur expedita labore maiores ad exercitationem nisi, molestias tenetur? Eos asperiores iure libero unde obcaecati incidunt adipisci molestiae ex. Distinctio, sapiente animi quod, incidunt sed quasi consequuntur temporibus quia, alias a laborum. Tenetur suscipit quas vero recusandae repudiandae sunt voluptate animi est reprehenderit, officia repellendus eveniet cum voluptatum rerum aspernatur. Ipsa laboriosam animi quas doloribus id cumque sapiente recusandae accusamus! Deleniti accusantium nam consectetur ex molestias?",
+      dates: [
+        {
+          id: "1",
+          date: { month: "Mar", day: 6 },
+        },
+        {
+          id: "2",
+          date: { month: "Mar", day: 7 },
+        },
+        {
+          id: "3",
+          date: { month: "Mar", day: 8 },
+        },
+        {
+          id: "4",
+          date: { month: "Mar", day: 9 },
+        },
+        {
+          id: "5",
+          date: { month: "Mar", day: 10 },
+        },
+        {
+          id: "6",
+          date: { month: "Mar", day: 11 },
+        },
+        {
+          id: "7",
+          date: { month: "Mar", day: 12 },
+        },
+      ],
+      times: [
+        { id: "1", time: "06:00" },
+        { id: "2", time: "07:30" },
+        { id: "3", time: "08:00" },
+        { id: "4", time: "09:30" },
+        { id: "5", time: "10:30" },
+        { id: "6", time: "12:00" },
+        { id: "7", time: "13:00" },
+        { id: "8", time: "14:30" },
+        { id: "9", time: "15:30" },
+      ],
       seats: [
         { y: 0, x: 2 },
         { y: 1, x: 1 },
@@ -63,6 +145,47 @@ export const Seats = () => {
       imgUrl: "/films/minions.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod et tempora nulla ad eos adipisci! Similique adipisci ea libero, magni harum vitae delectus accusamus omnis tempora temporibus esse asperiores deserunt ab dolore reiciendis aspernatur expedita labore maiores ad exercitationem nisi, molestias tenetur? Eos asperiores iure libero unde obcaecati incidunt adipisci molestiae ex. Distinctio, sapiente animi quod, incidunt sed quasi consequuntur temporibus quia, alias a laborum. Tenetur suscipit quas vero recusandae repudiandae sunt voluptate animi est reprehenderit, officia repellendus eveniet cum voluptatum rerum aspernatur. Ipsa laboriosam animi quas doloribus id cumque sapiente recusandae accusamus! Deleniti accusantium nam consectetur ex molestias?",
+      dates: [
+        {
+          id: "1",
+          date: { month: "Mar", day: 6 },
+        },
+        {
+          id: "2",
+          date: { month: "Mar", day: 7 },
+        },
+        {
+          id: "3",
+          date: { month: "Mar", day: 8 },
+        },
+        {
+          id: "4",
+          date: { month: "Mar", day: 9 },
+        },
+        {
+          id: "5",
+          date: { month: "Mar", day: 10 },
+        },
+        {
+          id: "6",
+          date: { month: "Mar", day: 11 },
+        },
+        {
+          id: "7",
+          date: { month: "Mar", day: 12 },
+        },
+      ],
+      times: [
+        { id: "1", time: "06:00" },
+        { id: "2", time: "07:30" },
+        { id: "3", time: "08:00" },
+        { id: "4", time: "09:30" },
+        { id: "5", time: "10:30" },
+        { id: "6", time: "12:00" },
+        { id: "7", time: "13:00" },
+        { id: "8", time: "14:30" },
+        { id: "9", time: "15:30" },
+      ],
       seats: [
         { y: 2, x: 0 },
         { y: 2, x: 1 },
@@ -85,6 +208,47 @@ export const Seats = () => {
       imgUrl: "/films/minions.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod et tempora nulla ad eos adipisci! Similique adipisci ea libero, magni harum vitae delectus accusamus omnis tempora temporibus esse asperiores deserunt ab dolore reiciendis aspernatur expedita labore maiores ad exercitationem nisi, molestias tenetur? Eos asperiores iure libero unde obcaecati incidunt adipisci molestiae ex. Distinctio, sapiente animi quod, incidunt sed quasi consequuntur temporibus quia, alias a laborum. Tenetur suscipit quas vero recusandae repudiandae sunt voluptate animi est reprehenderit, officia repellendus eveniet cum voluptatum rerum aspernatur. Ipsa laboriosam animi quas doloribus id cumque sapiente recusandae accusamus! Deleniti accusantium nam consectetur ex molestias?",
+      dates: [
+        {
+          id: "1",
+          date: { month: "Mar", day: 6 },
+        },
+        {
+          id: "2",
+          date: { month: "Mar", day: 7 },
+        },
+        {
+          id: "3",
+          date: { month: "Mar", day: 8 },
+        },
+        {
+          id: "4",
+          date: { month: "Mar", day: 9 },
+        },
+        {
+          id: "5",
+          date: { month: "Mar", day: 10 },
+        },
+        {
+          id: "6",
+          date: { month: "Mar", day: 11 },
+        },
+        {
+          id: "7",
+          date: { month: "Mar", day: 12 },
+        },
+      ],
+      times: [
+        { id: "1", time: "06:00" },
+        { id: "2", time: "07:30" },
+        { id: "3", time: "08:00" },
+        { id: "4", time: "09:30" },
+        { id: "5", time: "10:30" },
+        { id: "6", time: "12:00" },
+        { id: "7", time: "13:00" },
+        { id: "8", time: "14:30" },
+        { id: "9", time: "15:30" },
+      ],
       seats: [
         { y: 1, x: 3 },
         { y: 2, x: 1 },
@@ -113,6 +277,47 @@ export const Seats = () => {
       imgUrl: "/films/minions.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod et tempora nulla ad eos adipisci! Similique adipisci ea libero, magni harum vitae delectus accusamus omnis tempora temporibus esse asperiores deserunt ab dolore reiciendis aspernatur expedita labore maiores ad exercitationem nisi, molestias tenetur? Eos asperiores iure libero unde obcaecati incidunt adipisci molestiae ex. Distinctio, sapiente animi quod, incidunt sed quasi consequuntur temporibus quia, alias a laborum. Tenetur suscipit quas vero recusandae repudiandae sunt voluptate animi est reprehenderit, officia repellendus eveniet cum voluptatum rerum aspernatur. Ipsa laboriosam animi quas doloribus id cumque sapiente recusandae accusamus! Deleniti accusantium nam consectetur ex molestias?",
+      dates: [
+        {
+          id: "1",
+          date: { month: "Mar", day: 6 },
+        },
+        {
+          id: "2",
+          date: { month: "Mar", day: 7 },
+        },
+        {
+          id: "3",
+          date: { month: "Mar", day: 8 },
+        },
+        {
+          id: "4",
+          date: { month: "Mar", day: 9 },
+        },
+        {
+          id: "5",
+          date: { month: "Mar", day: 10 },
+        },
+        {
+          id: "6",
+          date: { month: "Mar", day: 11 },
+        },
+        {
+          id: "7",
+          date: { month: "Mar", day: 12 },
+        },
+      ],
+      times: [
+        { id: "1", time: "06:00" },
+        { id: "2", time: "07:30" },
+        { id: "3", time: "08:00" },
+        { id: "4", time: "09:30" },
+        { id: "5", time: "10:30" },
+        { id: "6", time: "12:00" },
+        { id: "7", time: "13:00" },
+        { id: "8", time: "14:30" },
+        { id: "9", time: "15:30" },
+      ],
       seats: [
         { y: 0, x: 2 },
         { y: 1, x: 2 },
