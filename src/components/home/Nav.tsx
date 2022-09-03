99c85477e4c0e639adf5cccf8a91db56f3484d67
Amr Taha <amrshiha2@gmail.com>
2022-09-03 14:44:46 +0200

    adding links to nav items

--- src/components/home/Nav.tsx
+++ src/components/home/Nav.tsx
@@ -1,19 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { MdHomeFilled } from "react-icons/md";
 import { BiCompass } from "react-icons/bi";
 import { HiTicket } from "react-icons/hi";
 import { BsFillPersonFill } from "react-icons/bs";
 
 export const Nav = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="py-4 px-14 fixed bottom-0 w-screen bg-[#111] rounded-3xl">
       <ul className="flex justify-between">
-        <li className="cursor-pointer ">
+        <li onClick={() => navigate("/")} className="cursor-pointer ">
           <MdHomeFilled className="text-5xl p-[10px]" />
         </li>
         <li className="cursor-pointer opacity-40">
           <BiCompass className="text-5xl p-[10px]" />
         </li>
-        <li className="cursor-pointer opacity-40">
+        <li
+          onClick={() => navigate("/my-tickets")}
+          className="cursor-pointer opacity-40"
+        >
           <HiTicket className="text-5xl p-[10px]" />
         </li>
         <li className="cursor-pointer opacity-40">
